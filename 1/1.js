5b0e2baa995941ae5c7f1b2ccdcc4aaf5cd8590c
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-06-20 23:44:05 +0300

    ch2

---
+++ 1/1.js
@@ -0,0 +1,30 @@
+"use strict"
+// alert( 'Оп нежданчик');
+// alert('Op nezhdanchik number two');
+// //commentary etcetra
+
+// let message;
+// message = 'Nezhdan';
+// alert(message);
+
+
+// let msg = 'NN', usr = 'Jhn', age = 22;
+// const bsd = 10;
+
+// alert(msg+bsd);
+//alert( Infinity ); // Infinity
+// let name = 'Jin';
+// alert(`Hello, ${name}`);
+// let nameFieldChecked = true; // да, поле отмечено
+// let ageFieldChecked = false;
+// let age = null;
+// age = undefined;
+// age = 5;
+// alert(typeof(age))
+
+// let age = prompt('Сколько тебе лет?', 100);
+
+// alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+let isBoss = confirm("Ты здесь главный?");
+
+alert( isBoss ); // true, если нажата OK
